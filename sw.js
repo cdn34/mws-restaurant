4fe682c8a549818ffc2c35f8fc75cc49b10b8cc5
Carlos Nogueira <carlosnogueira04@Carloss-MacBook-Pro.local>
2018-03-31 16:51:21 +0100

    Added service worker

---
+++ sw.js
@@ -0,0 +1,56 @@
+const currentCacheName = 'mws-restaurant-1';
+
+self.addEventListener('install', event => {
+    let imgArray = []
+    for(let i = 0; i < 10; i++ )
+        imgArray = imgArray.concat(`img/${i+1}.jpg`);
+
+    event.waitUntil(
+        caches.open(currentCacheName).then(cache => {
+
+            const requestCSS = new Request('https://normalize-css.googlecode.com/svn/trunk/normalize.css', {mode: 'no-cors'});
+            fetch(requestCSS).then(response => cache.put(requestCSS, response));
+
+            const requestMaps = new Request('https://maps.googleapis.com/maps/api/js?key=AIzaSyA2s2CKiGPYkP7R6UWtyBtOBXRZsf7Y5_c&libraries=places&callback=initMap', {mode: 'no-cors'});
+            fetch(requestMaps).then(response => cache.put(requestMaps, response));
+
+            return cache.addAll([
+                '/',
+                'restaurant.html',
+                'css/styles.css',
+                'data/restaurants.json',
+                'js/dbhelper.js',
+                'js/main.js',
+                'js/restaurant_info.js',
+            ].concat(imgArray));
+        })
+    );
+});
+
+self.addEventListener('activate', event => {
+    event.waitUntil(
+        caches.keys().then(cacheNames => {
+            Promise.all(
+                cacheNames.filter(cacheName => 
+                    cacheName.startsWith('mws-restaurant-') && cacheName !== currentCacheName
+                ).map(cacheName => caches.delete(cacheName))
+            )
+        })
+    )
+});
+
+self.addEventListener('fetch', event => {
+
+    const requestUrl = new URL(event.request.url);
+    if(requestUrl.pathname === '/restaurant.html') {
+        event.respondWith(caches.match('restaurant.html'));
+        return;
+    }
+
+    event.respondWith(caches.match(event.request).then(response => {
+        if(!response)
+            return fetch(event.request);
+        else return response;
+    }));
+});
+
